e8a57675b205924401145e913822aa39de4e19f4
okan.eren.bilgir <okanbilgir@hotmail.com>
2022-08-01 12:58:34 +0300

    loading screen added.

--- app/src/main/java/com/example/tasktrackerapp/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/tasktrackerapp/firebase/FirestoreClass.kt
@@ -9,25 +9,5 @@ class FirestoreClass {
 
     private val mFireStore = FirebaseFirestore.getInstance()
 
-    fun registerUser(activity: RegisterActivity, userInfo: User){
-        //users are stored under the name of "users" collection in firestore
-        mFireStore.collection(Constants.USERS)
-            .document(userInfo.id) //set user's unique id as document id
-            //pull all info entered by user, merge them together, send it firestore
-            .set(userInfo, SetOptions.merge())
-            //if registration is successful
-            .addOnSuccessListener {
-                //call basic activity and transfer results
-                activity.userRegistrationSuccess()
-            }
-            //if registration is not successful
-            .addOnFailureListener { e ->
-                activity.hideProgressDialog()
 
-                Log.e(activity.javaClass.simpleName,
-                    "registerUser: Error occurred.",
-                    e
-                )
-            }
-    }
 }
